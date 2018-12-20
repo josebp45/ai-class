209959752857b92946ed7d7f790363a719d220c8
goott6-30 <goott6-30@goott6-30-PC>
2018-12-20 12:44:15 +0900

    fgn

--- git-demo-joseph/src/TestMain.java
+++ git-demo-joseph/src/TestMain.java
@@ -3,6 +3,8 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		System.out.println("ㅋㅋsdfsdfㅋ");
+		System.out.println("ㅋㅋsdfsdfㅋ");
+		System.out.println("ㅋㅋsdfsdfㅋ");
 	}
 
 }
